c8c3aee296b1afd67c6a1ff6a85f565cc02875e9
zidane168 <huuvi168@gmail.com>
2018-07-22 13:46:43 +0700

    - split to every controller files by injected appMain to angular.module app

--- SinglePageRouterDemo/SinglePageRouterDemo/www/Controller/mainController.js
+++ SinglePageRouterDemo/SinglePageRouterDemo/www/Controller/mainController.js
@@ -1,6 +1,6 @@
 
-//var app = angular.module('app', []);
-//app.controller('mainController', function ($scope) {
-//    $scope.firstName = "Zidane";
-//    $scope.lastName = "Lee"
-//});
+var app = angular.module('appMain', []);
+app.controller('mainController', function ($scope) {
+    $scope.firstName = "Zidane YEAH";
+    $scope.lastName = "Lee"
+});
--- SinglePageRouterDemo/SinglePageRouterDemo/www/Controller/secondController.js
+++ SinglePageRouterDemo/SinglePageRouterDemo/www/Controller/secondController.js
@@ -1,7 +1,7 @@
 
 
-//var app = angular.module('app', []);
-//app.controller('mainController', function ($scope) {
-//    $scope.mobilePhone = "0906 440 368";
-//    $scope.email = "huuvi168@gmail.com";
-//});
+var app = angular.module('appSecond', []);
+app.controller('secondController', function ($scope) {
+    $scope.mobilePhone = "0906 440 368";
+    $scope.email = "huuvi168@gmail.com";
+});
